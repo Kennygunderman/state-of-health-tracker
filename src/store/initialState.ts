cc279d4034536852a296bcf9f4f63173f215092a
kennygunderman <kennygunderman@gmail.com>
2023-11-07 10:16:29 -0500

    fix typo in initialstate

--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -20,7 +20,7 @@ const localStore: LocalStore = {
         foods: [
             createFood('Chicken Breast (4oz)', 1, '176', { protein: 35, carbs: 0, fat: 4 }),
             createFood('Egg (Large)', 1, '78', { protein: 6, carbs: 0, fat: 6 }),
-            createFood('Peanut Butter (2 tbsp)', 1, '78', { protein: 7, carbs: 4, fat: 16 }),
+            createFood('Peanut Butter (2 tbsp)', 1, '180', { protein: 7, carbs: 4, fat: 16 }),
             createFood('Apple (Medium)', 1, '96', { protein: 0, carbs: 24, fat: 0 }),
         ],
     },
