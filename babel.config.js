e5cc26b211d8f94e37beab770dc8df4f865c45e9
kennygunderman <kennygunderman@gmail.com>
2024-09-07 10:49:14 -0400

    Add .env support

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,14 @@ module.exports = (api) => {
                 },
             ],
             'react-native-reanimated/plugin',
+            [
+                'module:react-native-dotenv',
+                {
+                    moduleName: '@env',
+                    path: '.env',
+                    allowlist: ['USDA_BASE_URL', 'USDA_FOOD_API_KEY'],
+                },
+            ],
         ],
     };
 };
---
+++ env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+    export const USDA_BASE_URL: string;
+    export const USDA_FOOD_API_KEY: string;
+}
