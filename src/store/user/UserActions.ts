baeb77dbaa918f4613b00d66b6c93d5f33a960e7
kennygunderman <kennygunderman@gmail.com>
2025-06-23 15:16:27 -0400

    Sync user data is older than day

--- src/store/user/UserActions.ts
+++ src/store/user/UserActions.ts
@@ -2,6 +2,8 @@ import userService from '@service/user/UserService'
 import {setMealEntriesSynced} from '@store/dailyMealEntries/DailyMealEntriesActions'
 import LocalStore from '@store/LocalStore'
 
+import {isDateOlderThanADay} from '../../utility/DateUtility'
+
 export const LOG_IN_USER: string = 'LOG_IN_USER'
 
 export const LOG_OUT_USER: string = 'LOG_OUT_USER'
@@ -19,9 +21,9 @@ export function syncUserData(userId: string) {
   return async (dispatch: any, getState: () => LocalStore) => {
     // only sync logged-in users if last data sync was > 1 day ago.
     const {user} = getState()
-    // if (!isDateOlderThanADay(user.lastDataSync ?? 0)) {
-    //     return;
-    // }
+    if (!isDateOlderThanADay(user.lastDataSync ?? 0)) {
+      return
+    }
 
     await userService.saveUserData(userId, getState()).then(() => {
       dispatch(setMealEntriesSynced())
